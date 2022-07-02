ab52b41f172d6007ac7f3c3455a310229dd7ceeb
Yuichi Sugita <34782651+cedro3@users.noreply.github.com>
2022-07-02 18:14:53 +0900

    Update function.py

--- function.py
+++ function.py
@@ -83,6 +83,28 @@ def display_pic(folder):
     plt.show()
     plt.close()
 
+
+# --- display_pic_png ---
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+import os
+import glob
+
+def display_pic_png(folder):
+    fig = plt.figure(figsize=(30, 60))
+    files = sorted(glob.glob(folder+'/*.png'))
+    for i, file in enumerate(files):
+        img = Image.open(file)    
+        images = np.asarray(img)
+        ax = fig.add_subplot(10, 5, i+1, xticks=[], yticks=[])
+        image_plt = np.array(images)
+        ax.imshow(image_plt)
+        name = os.path.basename(file)
+        ax.set_xlabel(name, fontsize=30)  
+        fig.tight_layout()             
+    plt.show()
+    plt.close()
     
 # --- size adjust --- 
 import cv2
